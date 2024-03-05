439bca21cea10b3046f3e0b1d771881a3d7d50e0
Emilse Arias <emilsearias@gmail.com>
2024-03-05 17:24:26 -0300

    instalamos express y habilitamos los modulos

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+import express from 'express';
+
 console.log('hola mundo')
 console.log('hola mundo v2')
+
+//1 - configurar un puerto
+
+//2 - configurar los middlewares
+
+//3 - configurar las rutas
