b0990e4bbe550c27f1fbb694bbcf2df99c800c1b
Emilse Arias <emilsearias@gmail.com>
2024-03-07 18:27:51 -0300

    agregamos la conexion con la base de datos

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import productosRouter from './src/routes/productos.routes.js';
+import './src/database/database.js'
 
 //1 - configurar un puerto
 const app = express();
