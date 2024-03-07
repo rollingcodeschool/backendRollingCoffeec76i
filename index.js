f89c422d06078bda8a76ec5ce112427c2f4d28c6
Emilse Arias <emilsearias@gmail.com>
2024-03-07 17:01:25 -0300

    configuramos un archivo estatico en la ruta principal del backend

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import 'dotenv/config'; // permite procesar variables de entorno
 import cors from 'cors';
 import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
 //1 - configurar un puerto
 const app = express();
 
@@ -12,11 +14,17 @@ app.listen(app.get('port'), ()=>{
 //2 - configurar los middlewares
 app.use(cors()); // permite obtener conexiones remotas
 app.use(morgan('dev'))// nos da info extra en la terminal
-//todo: agregar el resto de los middlewares
+app.use(express.json()); //interpreta los datos en formato json
+app.use(express.urlencoded({extended:true})) //interpreta datos del body del request
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// console.log(__filename)
+// console.log(path.join(__dirname,'/public'))
+app.use(express.static(path.join(__dirname,'/public')))
 
 //3 - configurar las rutas
-//http://localhost:3000/productos
-app.get('/', (req, res)=>{
+//http://localhost:4001/
+app.get('/nuevo', (req, res)=>{
     //agregar toda la logica
     console.log('procesando una solicitud get');
     res.send('Respuesta del backend rollingcoffee')
