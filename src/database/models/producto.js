beaacb89dea78f23f1458b2d6d901ca525ef0082
Emilse Arias <emilsearias@gmail.com>
2024-03-12 17:31:40 -0300

    creamos el Schema de productos

---
+++ src/database/models/producto.js
@@ -0,0 +1,44 @@
+import mongoose, {Schema} from "mongoose";
+
+const productoSchema = new Schema({
+    nombreProducto:{
+        type: String,
+        required:true,
+        minLength: 2,
+        maxLength:50,
+        unique:true
+    },
+    precio:{
+        type:Number,
+        requider:true,
+        min: 50,
+        max: 10000
+    },
+    imagen:{
+        type:String,
+        required:true,
+        validate: {
+            validator: (valor)=>{
+               return /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/.test(valor)
+            } ,
+            message: dato => `${dato.value} no es una URL de imagen valida` 
+        }
+    },
+    categoria:{
+        type:String,
+        required:true,
+        enum: ['Infusiones', 'Batidos','Dulce', 'Salado']
+    },
+    descripcion_breve:{
+        type:String,
+        required:true,
+        minLength:3,
+        maxLength:30
+    },
+    descripcion_amplia:{
+        type:String,
+        required:true,
+        minLength:50,
+        maxLength:1000
+    },
+})
