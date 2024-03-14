106aea4e18953f42be5f4f24358617ce4700f405
Emilse Arias <emilsearias@gmail.com>
2024-03-14 17:17:57 -0300

    agregamos la funcion obtener producto la cual usa un id por parametro

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -30,3 +30,21 @@ export const crearProducto = async(req, res)=>{
         })
     }
 }
+
+export const obtenerProducto = async(req,res)=>{
+    try {
+        //extraer el id
+        console.log(req.params.id)
+        //solicitar a la bd buscar ese producto
+        const productoBuscado = await Producto.findById(req.params.id)
+        //preguntar si no encontre el producto buscado
+        if(!productoBuscado){
+            return res.status(404).json({mensaje:"El producto con el id enviado no existe"})
+        }
+        //enviar respuesta
+        res.status(200).json(productoBuscado);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({mensaje: "No se pudo encontrar el producto solicitado"})
+    }
+}
